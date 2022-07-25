6484dc7a910a1a6393bc2a33a92969712249da5e
Albert <thegoversus@gmail.com>
2022-07-25 13:42:22 +0300

    models and misc commit

--- django_study/news/models.py
+++ django_study/news/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
-# Create your models here.
+
+class News(models.Model):
+    title = models.CharField(max_length=150)  # title = Varchar
+    content = models.TextField(blank=True)  # content = Text
+    created_at = models.DateTimeField(auto_now_add=True)  # created_at = DateTime
+    updated_at = models.DateTimeField(auto_now=True)  # updated_at = DateTime
+    photo = models.ImageField(upload_to='photos/%Y/%m/%d/')  # photo = Image
+    is_published = models.BooleanField(default=True)  # is_published = bool
--- django_study/test.py
+++ django_study/test.py
@@ -1,9 +1,8 @@
-x=2
-
-
 class Test:
     def __init__(self):
         print('hello')
 
+
 if __name__ == '__main__':
     a = Test()
+    print('АХАХАХАХААХХАХАХА')
